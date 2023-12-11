7de8f86203c5831a27a70f67197ed3626191a777
muesli4brekkes <muesli4brekkies@gmail.com>
2023-12-11 21:01:39 +0000

    day1

---
+++ day1/src/main.rs
@@ -0,0 +1,21 @@
+use std::fs;
+fn main() {
+    println!(
+        "{}",
+        fs::read_to_string("./aoc_1_input.txt")
+            .unwrap()
+            .split("\n")
+            .filter_map(|e| if e.len() > 0 {
+                Some(e.chars().filter(|c| c.is_numeric()).collect::<Vec<_>>())
+            } else {
+                None
+            })
+            .map(|c| {
+                format!("{}{}", c.first().unwrap(), c.last().unwrap())
+                    .parse::<i32>()
+                    .unwrap()
+            })
+            .reduce(|a, b| a + b)
+            .unwrap()
+    )
+}
