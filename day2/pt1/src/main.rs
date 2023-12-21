7bc2fbc9d90534eff2c39a66fee1b3569ca037f3
muesli4brekkes <muesli4brekkies@gmail.com>
2023-12-21 03:45:54 +0000

    fixes

--- day2/pt1/src/main.rs
+++ day2/pt1/src/main.rs
@@ -11,33 +11,35 @@ const LIMS: Cols = Cols {
 };
 
 fn main() {
-    let result = fs::read_to_string("/home/muesli/git/aoc_2023/day2/input.txt")
+    let result: i32 = fs::read_to_string("/home/muesli/git/aoc_2023/day2/input.txt")
         .unwrap()
         .lines()
-        .map(|e| {
-            e.split(|c| c == ';' || c == ':' || c == ',')
+        .filter_map(|line| {
+            if line
+                .split(|c| c == ';' || c == ':' || c == ',')
                 .map(str::trim)
-                .collect::<Vec<_>>()
+                .all(|string| {
+                    let words: Vec<&str> = string.split(' ').collect();
+                    let (n, colour) = (words[0].parse::<i32>().unwrap_or(0), words[1]);
+                    match colour {
+                        "red" => n <= LIMS.red,
+                        "blue" => n <= LIMS.blue,
+                        "green" => n <= LIMS.green,
+                        _ => true,
+                    }
+                })
+            {
+                Some(
+                    line.split(' ').collect::<Vec<&str>>()[1]
+                        .replace(':', "")
+                        .parse::<i32>()
+                        .unwrap(),
+                )
+            } else {
+                None
+            }
         })
-        .filter(|strings| {
-            strings.iter().all(|string| {
-                let words: Vec<&str> = string.split(' ').collect();
-                let (n, colour) = (words[0].parse::<i32>().unwrap_or(0), words[1]);
-                match colour {
-                    "red" => n <= LIMS.red,
-                    "blue" => n <= LIMS.blue,
-                    "green" => n <= LIMS.green,
-                    _ => true,
-                }
-            })
-        })
-        .map(|c| {
-            c[0].split(' ').collect::<Vec<_>>()[1]
-                .parse::<i32>()
-                .unwrap()
-        })
-        .reduce(|a, b| a + b)
-        .unwrap();
+        .sum();
 
     println!("{}", result)
 }
