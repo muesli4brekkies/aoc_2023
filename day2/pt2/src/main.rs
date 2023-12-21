1401d2028bbd6cb0044087565296af3797937a23
muesli4brekkes <muesli4brekkies@gmail.com>
2023-12-21 02:59:10 +0000

    fixes

--- day2/pt2/src/main.rs
+++ day2/pt2/src/main.rs
@@ -1,27 +1,21 @@
 use std::fs;
-
 fn main() {
-    let result = fs::read_to_string("/home/muesli/git/aoc_2023/day2/input.txt")
-        .unwrap()
-        .lines()
-        .fold(0, |a, strings| {
-            a + ["red", "green", "blue"].into_iter().fold(1, |a, col| {
-                a * strings
+    println!(
+        "{}",
+        fs::read_to_string("/home/muesli/git/aoc_2023/day2/input.txt")
+            .unwrap()
+            .lines()
+            .fold(0, |a, strings| a + ["red", "green", "blue"]
+                .into_iter()
+                .fold(1, |a, col| a * strings
                     .split(|c| c == ';' || c == ':' || c == ',')
-                    .map(str::trim)
-                    .filter(|string| string.split(' ').collect::<Vec<&str>>()[1] == col)
                     .fold(0, |a, string| {
-                        let b = string.split(' ').collect::<Vec<&str>>()[0]
-                            .parse::<i32>()
-                            .unwrap();
-                        if b < a {
+                        let n = string[0..3].trim().parse::<i32>().unwrap_or(0);
+                        if n < a || !string.ends_with(col) {
                             a
                         } else {
-                            b
+                            n
                         }
-                    })
-            })
-        });
-
-    println!("{}", result)
+                    })))
+    )
 }
