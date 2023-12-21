0ddfcdaffb94fb8e916d53480394eadfaac594c0
muesli4brekkes <muesli4brekkies@gmail.com>
2023-12-21 02:17:30 +0000

    fixes

--- day2/pt2/src/main.rs
+++ day2/pt2/src/main.rs
@@ -1,23 +1,8 @@
 use std::fs;
-fn findmin(col: &str, strings: &[&str]) -> i32 {
-    strings
-        .iter()
-        .filter(|string| string.split(' ').collect::<Vec<&str>>()[1] == col)
-        .fold(0, |a, string| {
-            let b = string.split(' ').collect::<Vec<&str>>()[0]
-                .parse::<i32>()
-                .unwrap();
-            if b < a {
-                a
-            } else {
-                b
-            }
-        })
-}
 
 fn main() {
-    let file_string = fs::read_to_string("/home/muesli/git/aoc_2023/day2/input.txt").unwrap();
-    let result: i32 = file_string
+    let result = fs::read_to_string("/home/muesli/git/aoc_2023/day2/input.txt")
+        .unwrap()
         .lines()
         .map(|e| {
             e.split(|c| c == ';' || c == ':' || c == ',')
@@ -25,9 +10,21 @@ fn main() {
                 .collect::<Vec<&str>>()
         })
         .fold(0, |a, strings| {
-            a + ["red", "green", "blue"]
-                .iter()
-                .fold(1, |a, col| a * findmin(col, &strings))
+            a + ["red", "green", "blue"].into_iter().fold(1, |a, col| {
+                a * strings
+                    .iter()
+                    .filter(|string| string.split(' ').collect::<Vec<&str>>()[1] == col)
+                    .fold(0, |a, string| {
+                        let b = string.split(' ').collect::<Vec<&str>>()[0]
+                            .parse::<i32>()
+                            .unwrap();
+                        if b < a {
+                            a
+                        } else {
+                            b
+                        }
+                    })
+            })
         });
 
     println!("{}", result)
