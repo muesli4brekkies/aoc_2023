6175638ddcf5f1eb9ce25b452ab414bcb16cd50b
muesli4brekkes <muesli4brekkies@gmail.com>
2023-12-21 02:18:39 +0000

    fixes

--- day2/pt2/src/main.rs
+++ day2/pt2/src/main.rs
@@ -4,15 +4,11 @@ fn main() {
     let result = fs::read_to_string("/home/muesli/git/aoc_2023/day2/input.txt")
         .unwrap()
         .lines()
-        .map(|e| {
-            e.split(|c| c == ';' || c == ':' || c == ',')
-                .map(str::trim)
-                .collect::<Vec<&str>>()
-        })
         .fold(0, |a, strings| {
             a + ["red", "green", "blue"].into_iter().fold(1, |a, col| {
                 a * strings
-                    .iter()
+                    .split(|c| c == ';' || c == ':' || c == ',')
+                    .map(str::trim)
                     .filter(|string| string.split(' ').collect::<Vec<&str>>()[1] == col)
                     .fold(0, |a, string| {
                         let b = string.split(' ').collect::<Vec<&str>>()[0]
