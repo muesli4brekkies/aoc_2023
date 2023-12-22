d0181fb927214528f005a04519506a2f32246996
muesli4brekkes <muesli4brekkies@gmail.com>
2023-12-22 12:25:58 +0000

    f

--- day3/pt1/target/debug/deps/pt1-610b84929a27b7a2.d
+++
@@ -1,5 +0,0 @@
-/home/muesli/git/aoc_2023/day3/pt1/target/debug/deps/pt1-610b84929a27b7a2: src/main.rs
-
-/home/muesli/git/aoc_2023/day3/pt1/target/debug/deps/pt1-610b84929a27b7a2.d: src/main.rs
-
-src/main.rs:
--- day3/pt1/target/debug/pt1.d
+++
@@ -1,1 +0,0 @@
-/home/muesli/git/aoc_2023/day3/pt1/target/debug/pt1: /home/muesli/git/aoc_2023/day3/pt1/src/main.rs
