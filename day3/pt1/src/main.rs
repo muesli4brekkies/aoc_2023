f82adf9f53a4237b1397b3800d2dcd38a41ebf60
muesli4brekkes <muesli4brekkies@gmail.com>
2023-12-22 22:01:07 +0000

    added readme

--- day3/pt1/src/main.rs
+++ day3/pt1/src/main.rs
@@ -3,39 +3,36 @@ use std::fs;
 fn main() {
     let file_str = fs::read_to_string("/home/muesli/git/aoc_2023/day3/input.txt").unwrap();
     let file_lines = file_str.lines().collect::<Vec<&str>>();
-    let line_width = file_str.lines().collect::<Vec<_>>()[0].len();
+    let line_width = file_lines[0].len();
     let result: i32 = file_str.lines().enumerate().fold(0, |a, line| {
         a + {
             let mut i = 0;
             line.1
                 .char_indices()
                 .filter_map(move |c| {
-                    if i <= c.0 {
-                        if c.1.is_numeric() {
-                            let j = i;
-                            i += 3;
-                            Some((
-                                j,
-                                line.1[j..if j + 3 > line_width {
-                                    line_width
-                                } else {
-                                    j + 3
-                                }]
-                                    .chars()
-                                    .filter(|c| c.is_numeric())
-                                    .collect::<String>(),
-                            ))
-                        } else {
-                            i += 1;
-                            None
-                        }
-                    } else {
+                    if i > c.0 {
                         None
+                    } else if !c.1.is_numeric() {
+                        i += 1;
+                        None
+                    } else {
+                        i += 3;
+                        Some((
+                            c.0,
+                            line.1[c.0..if c.0 + 3 > line_width {
+                                line_width
+                            } else {
+                                c.0 + 3
+                            }]
+                                .chars()
+                                .filter(|c| c.is_numeric())
+                                .collect::<String>(),
+                        ))
                     }
                 })
                 .fold(0, |a, n| {
                     a + {
-                        let range = if n.0 as i32 - 1 > 0 { n.0 - 1 } else { 0 }
+                        let range = if n.0 == 0 { 0 } else { n.0 - 1 }
                             ..if n.0 + n.1.len() < line_width {
                                 n.0 + n.1.len() + 1
                             } else {
@@ -44,13 +41,10 @@ fn main() {
                         if format!(
                             "{}{}{}",
                             &line.1[range.clone()],
-                            &match file_lines.get((line.0 as i32 - 1) as usize) {
-                                Some(r) => r.to_string(),
-                                None => ".".repeat(line_width),
-                            }[range.clone()],
+                            &file_lines[if line.0 == 0 { 0 } else { line.0 - 1 }][range.clone()],
                             &match file_lines.get(line.0 + 1) {
-                                Some(r) => r.to_string(),
-                                None => ".".repeat(line_width),
+                                Some(r) => r,
+                                None => line.1,
                             }[range.clone()],
                         )
                         .contains(|c: char| c != '.' && !c.is_numeric())
